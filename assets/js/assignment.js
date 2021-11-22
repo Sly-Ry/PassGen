aee19dde02b5e30bbfeda22cc231346fa8efda99
ryan <ryan.williams93@aol.com>
2021-11-21 21:09:50 -0600

    add starter variables

--- assets/js/assignment.js
+++ assets/js/assignment.js
@@ -1,4 +1,9 @@
 // Assignment code here
+// input variables
+lettersLow = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+lettersUpp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+character = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '[', '}', '{', '}', '\\',  '|', '"', ';', ':', '/', '?', '.', '<', '>', ',', '`', '~'];
 
 
 // Get references to the #generate element
@@ -6,6 +11,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  enter = parsInt(prompt("Choose the length of your password. It must bewteen 8 and 128 characters."));
+  if (enter < 8 || enter > 128) {
+    enter = parseInt("Pick a length bewteen 8 128 charcters, ya silly.")
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +24,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+writePassword();
